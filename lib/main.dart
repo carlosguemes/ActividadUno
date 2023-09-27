2906841f6be6c38e3eb1042b644a699941cfb748
carlos <carlosguemesdam@gmail.com>
2023-09-27 17:58:01 +0200

    Commit V2 - Conexion con firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'App.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+
+void main() async{
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(App());
+
+
 }
 
 
