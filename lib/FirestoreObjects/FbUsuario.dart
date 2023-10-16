4782a56c3e077836e0958e7cd8743df1a5f3e099
carlos <carlosguemesdam@gmail.com>
2023-10-16 18:02:52 +0200

    Practica 2: Commit V4 - Creamos la clase FbUsuario donde guardaremos y recogeremos datos de la base de datos

---
+++ lib/FirestoreObjects/FbUsuario.dart
@@ -0,0 +1,14 @@
+class FbUsuario {
+
+  final String nombre;
+  final int edad;
+  final double altura;
+  final String colorPelo;
+
+  FbUsuario({
+    required this.nombre,
+    required this.edad,
+    required this.altura,
+    required this.colorPelo
+  });
+}
