12e4bf98fd5291d581c3385b65f622748f43c3af
carlos <carlosguemesdam@gmail.com>
2023-10-17 18:15:27 +0200

    Practica 2: Commit V14 - Añadimos el método para descargar los Posts en el HomeView

--- lib/OnBoarding/HomeView.dart
+++ lib/OnBoarding/HomeView.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../FirestoreObjects/FbPost.dart';
 
 class HomeView extends StatefulWidget {
 
@@ -15,36 +18,32 @@ class _HomeViewState extends State<HomeView> {
     Navigator.of(_context).popAndPushNamed('/loginview');
   }
 
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  final List<FbPost> post = [];
+
+  @override
+  void initState() {
+    descargarPosts();
+    super.initState();
+  }
+
+  void descargarPosts() async{
+    CollectionReference<FbPost> reference = db
+        .collection("Posts")
+        .withConverter(fromFirestore: FbPost.fromFirestore,
+        toFirestore: (FbPost post, _) => post.toFirestore());
+
+    QuerySnapshot<FbPost> querySnap = await reference.get();
+    for (int i = 0; i < querySnap.docs.length; i++){
+      setState(() {
+        post.add(querySnap.docs[i].data());
+      });
+
+    }
+}
+
   @override
   Widget build(BuildContext context) {
-    _context = context;
-
-    Column columna = Column(children: [
-      Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-      Text("Bienvenido a Kyty", style: TextStyle(fontSize: 25)),
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 700, vertical: 10),),
-
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        TextButton(onPressed: onClickVolver, child: Text("Volver a login")),
-      ],),
-    ],); //Columna
-
-
-    AppBar appBar = AppBar(
-      title: const Text('Home'),
-      centerTitle: true,
-      shadowColor: Colors.blue,
-      backgroundColor: Colors.black,
-    );
-
-    Scaffold scaf = Scaffold(
-      body: columna,
-      backgroundColor: Colors.amberAccent,
-      appBar: appBar,
-    );
-
-    return scaf;
     // TODO: implement build
     throw UnimplementedError();
   }
