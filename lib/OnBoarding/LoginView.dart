413cd12145f1394600fccd92d6e6e68d5c936924
carlos <carlosguemesdam@gmail.com>
2023-10-17 17:57:50 +0200

    Practica 2: Commit V8 - Modificamos la clase LoginView añadiendo condiciones en el botón de aceptar

--- lib/OnBoarding/LoginView.dart
+++ lib/OnBoarding/LoginView.dart
@@ -1,8 +1,10 @@
 import 'package:actividad_uno/App.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../FirestoreObjects/FbUsuario.dart';
 import '../KTPaddingText/PaddingClass.dart';
 
 class LoginView extends StatelessWidget {
@@ -12,13 +14,39 @@ class LoginView extends StatelessWidget {
   TextEditingController tecUsername = TextEditingController();
   TextEditingController tecPassword = TextEditingController();
 
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
   void onClickAceptar() async {
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: tecUsername.text,
         password: tecPassword.text,
       );
-      Navigator.of(_context).popAndPushNamed('/homeview');
+
+      String uidUsuario = FirebaseAuth.instance.currentUser!.uid;
+
+      DocumentReference<FbUsuario> reference = db
+          .collection("Usuarios")
+          .doc(uidUsuario)
+          .withConverter(fromFirestore: FbUsuario.fromFirestore,
+          toFirestore: (FbUsuario usuario, _) => usuario.toFirestore());
+
+      DocumentSnapshot<FbUsuario> docSnap = await reference.get();
+      if (docSnap.exists) {
+        FbUsuario usuario = docSnap.data()!;
+        if (usuario != null) {
+          print("Nombre del usuario: " + usuario.nombre);
+          print("Edad del usuario: " + usuario.edad.toString());
+          print("Altura del usuario: " + usuario.altura.toString());
+          print("Color de pelo del usuario: " + usuario.colorPelo);
+          Navigator.of(_context).popAndPushNamed("/homeview");
+        }
+      }
+
+      else{
+        Navigator.of(_context).popAndPushNamed('/perfilview');
+      }
+
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         print('No user found for that email.');
