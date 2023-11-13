0efea01062c1ac4ca5b90fce84a132ca202d0616
Carlos <carlosguemesdam@gmail.com>
2023-11-13 12:33:17 +0100

    Practica 3: Commit V12 - Creación de GestionView

---
+++ lib/OnBoarding/GestionView.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GestionView extends StatefulWidget {
+
+  @override
+  State<GestionView> createState() => _GestionViewState();
+}
+
+class _GestionViewState extends State<GestionView> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkSession();
+  }
+
+  void checkSession() async{
+    await Future.delayed(Duration(seconds: 2));
+      Navigator.of(context).popAndPushNamed("/splashview");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    AppBar appBar = AppBar(
+      centerTitle: true,
+      backgroundColor: Colors.black,
+      title: const Text('Gestión'),
+    );
+
+    Column columna = Column(children: [
+      Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+      Text("Bienvenido a Gestión de Kyty", style: TextStyle(fontSize: 25)),
+      Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+    ]);
+
+    Scaffold scaffold = Scaffold (
+      appBar: appBar,
+      body: columna,
+      backgroundColor: Colors.blueGrey,
+    );
+
+    return scaffold;
+  }
+}
