effe0d7aa721757a61a322b7178486394699a7ad
Carlos <carlosguemesdam@gmail.com>
2024-01-11 13:51:59 +0100

    Practica 4 -> V7: Creación de FirebaseAdmin

---
+++ lib/Singletone/FirebaseAdmin.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../FirestoreObjects/FbUsuario.dart';
+
+
+class FirebaseAdmin {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  FbUsuario? usuario;
+
+  Future<FbUsuario?> loadFbUsuario() async {
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+    DocumentReference<FbUsuario> ref = db.collection("Usuarios")
+        .doc(uid)
+        .withConverter(fromFirestore: FbUsuario.fromFirestore,
+      toFirestore: (FbUsuario usuario, _) => usuario.toFirestore(),);
+
+
+    DocumentSnapshot<FbUsuario> docSnap = await ref.get();
+    usuario = docSnap.data();
+    return usuario;
+  }
+
+  Future<List<Map<String, dynamic>>> searchPostsByTitle(String searchValue) async {
+    QuerySnapshot querySnapshot = await db
+        .collection('Posts')
+        .where('Titulo', isGreaterThanOrEqualTo: searchValue)
+        .get();
+
+    return querySnapshot.docs
+        .where((doc) =>
+    (doc['Titulo'] as String).contains(searchValue) ||
+        (doc['Usuario'] as String).contains(searchValue))
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
+  }
+
+}
